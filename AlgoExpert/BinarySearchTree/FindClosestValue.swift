9492027b21848f8db51a176ee75f765ddeed3821
Hirendra Sharma <hks.it11@gmail.com>
2024-02-04 14:07:09 +0530

    Add new method

--- AlgoExpert/BinarySearchTree/FindClosestValue.swift
+++ AlgoExpert/BinarySearchTree/FindClosestValue.swift
@@ -69,7 +69,7 @@ class FindClosestValue {
         }
         return closest
     }
-
+    
     func testMethod() {
         let bst =  FindClosestValue()
         var rootnode = bst.createBST()
@@ -86,6 +86,10 @@ class FindClosestValue {
     }
     
     func newMethodForBranch2() {
+        
+    }
+    
+    func newMethodForMainBranch() {
         
     }
 }
